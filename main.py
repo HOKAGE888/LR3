490bbf1cef162bb20145b1ae9eecc096819b6798
HOKAGE_8 <gozhevaKatya@yandex.ru>
2022-01-30 22:37:25 +0300

    muy

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+def collatz(x):
+    result=[]
+    while(x!=1):
+        result.append(x)
+        if(x%2==0):
+            x=x2(x)
+        else:
+            x=x3_1(x)
+    result.append(x)
+    return result
